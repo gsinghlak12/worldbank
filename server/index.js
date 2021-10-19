fe5ce54e2641250f3c88da4c09013d2ff9a8ebe0
gsinghlak12 <gsinghlak12@gmail.com>
2021-10-19 19:01:36 +0100

    working

--- server/index.js
+++ server/index.js
@@ -4,29 +4,52 @@ const path = require("path");
 const indicators = require("./routers/indicators");
 const users = require("./routers/users");
 const history = require("./routers/history");
-const bodyParser = require("body-parser");
-// const client2 = new Client("postgres://localhost:5432/worldbank");
+const sessions = require("./routers/sessions");
+const bcrypt = require("bcrypt");
+const v4 = require("uuid");
+const { Server } = require("http");
+const cookieSession = require("cookie-session");
+const e = require("express");
+const { resolveSoa } = require("dns");
+const cors = require("cors");
+
+// import { Pool } from "pg";
+
 const PORT = process.env.PORT || 8080;
 const app = express();
-const cors = require("cors");
+
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 app.use("/api/indicators", indicators);
 app.use("/api/users", users);
 app.use("/api/history", history);
+app.use("/api/sessions", sessions);
 
-app.get("/api", async (req, res) => {});
+// app.post("/users", async (req, res) => {});
+
+// app.post("/users/verify", async (req, res) => {});
+
+// app.post("/sessions", async (req, res) => {});
+
+// app.get("/sessions/check", async (req, res) => {});
+
+// app.get("sessions/delete", async (req, res) => {});
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+	console.log(`Server listening on ${PORT}`);
 });
