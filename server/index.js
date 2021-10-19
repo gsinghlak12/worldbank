b64a742612e27858e765bd5867204150805101fe
gsinghlak12 <gsinghlak12@gmail.com>
2021-10-19 11:33:37 +0100

    working remote-postgres

--- server/index.js
+++ server/index.js
@@ -1,31 +1,46 @@
 const express = require("express");
 const path = require("path");
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+const { Pool, Client } = require("pg");
+const client = new Client({
+	user: "doadmin",
+	host: "db-postgresql-lon1-54384-do-user-10062307-0.b.db.ondigitalocean.com",
+	database: "data",
+	password: "mEW3kfIjm7w9dnDG",
+	port: "25060",
+	sslmode: "require",
+	ssl: true,
+});
+
+(async function () {
+	await client.connect();
+})();
+
+app.get("/api", async (req, res) => {
+	const result = await client.query("SELECT * FROM Series LIMIT 5");
+	res.json(result.rows);
 });
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello World!" });
+	res.json({ message: "Hello World!" });
 });
 
 // Handle GET requests to /api route
 app.get("/api/hello2", (req, res) => {
-  res.json({ message: "Hello World!" });
+	res.json({ message: "Hello World!" });
 });
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+	console.log(`Server listening on ${PORT}`);
 });
