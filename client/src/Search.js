48f521afbbadb85c35d2c59cc8c92958b3c5ddeb
kashcoding <88490912+kashcoding@users.noreply.github.com>
2021-10-21 15:34:41 +0100

    Search styling finished

--- client/src/Search.js
+++ client/src/Search.js
@@ -103,21 +103,39 @@ function Search() {
     if (clicked) {
       return (
         <Button
-          className="mx-2"
+          className="m-1"
           variant="outline-secondary"
           onClick={(e) => cleanSecondCountry(e)}
         >
-          -
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            class="bi bi-dash"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+          </svg>{" "}
         </Button>
       );
     }
     return (
       <Button
-        className="mx-2"
+        className="m-1 "
         variant="outline-secondary"
         onClick={(e) => makeSecondCountryInput(e)}
       >
-        +
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="currentColor"
+          class="bi bi-plus "
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+        </svg>
       </Button>
     );
   };
@@ -134,54 +152,59 @@ function Search() {
 
   const hideSecondCountry = () => {
     if (clicked) {
-      return "btn btn-light dropdown-toggle";
+      return "input";
     }
     return "d-none";
   };
 
   const addNewCountryField = () => {
     return (
-      <div>
-        <input
-          className={hideSecondCountry()}
-          list="countryList2"
-          placeholder="Type a country..."
-          autoComplete="on"
-          onChange={(e) =>
-            validateInput(e.target.value, countryList, setSecondCountry)
-          }
-        ></input>
-        <datalist id="countryList2">{countryDropDown("second")}</datalist>
-      </div>
+      <Container className="d-flex flex-column align-items-center text-center">
+        <div className={hideSecondCountry()}>
+          <label className="p-2">Country 2:</label>
+          <input
+            className="btn btn-light dropdown-toggle m-1"
+            list="countryList2"
+            placeholder="Type a country..."
+            autoComplete="on"
+            onChange={(e) =>
+              validateInput(e.target.value, countryList, setSecondCountry)
+            }
+          ></input>
+          <datalist id="countryList2">{countryDropDown("second")}</datalist>
+        </div>
+      </Container>
     );
   };
 
   return (
     <div>
-      <Container className="container border border-secondary rounded d-flex align-content-center justify-content-center shadow py-4 px-5 bg-white rounded">
-        <Form className="d-flex flex-column align-items-center">
+      <Container className="container border border-secondary rounded d-flex shadow py-4 px-5 bg-white rounded">
+        <Form className="d-flex flex-column align-items-center justify-content-center">
           <Form.Text>
-            <h3 className="text-center pb-3">Search data</h3>
+            <h3 className="text-center pb-2">Search data</h3>
           </Form.Text>
-          <Container>
-            <Container>
-              <label>Countries:</label>
-              <input
-                class="btn btn-light dropdown-toggle"
-                list="countryList1"
-                placeholder="Type a country..."
-                onChange={(e) =>
-                  validateInput(e.target.value, countryList, setFirstCountry)
-                }
-              ></input>
+          <Container className="d-flex flex-row align-items-start justify-content-center">
+            <Container className="d-flex flex-row align-items-end justify-items-start">
               {addCountryButton()}
+              <Container className="d-flex flex-column align-items-center text-center">
+                <label className="p-2">Country 1:</label>
+                <input
+                  className="btn btn-light dropdown-toggle m-1"
+                  list="countryList1"
+                  placeholder="Type a country..."
+                  onChange={(e) =>
+                    validateInput(e.target.value, countryList, setFirstCountry)
+                  }
+                ></input>
+              </Container>
               {addNewCountryField()}
               <datalist id="countryList1">{countryDropDown("first")}</datalist>
             </Container>
-            <Container>
-              <label>Indicators:</label>
+            <Container className="d-flex flex-column align-items-center text-center">
+              <label className="p-2">Indicators:</label>
               <input
-                className="btn btn-light dropdown-toggle"
+                className="btn btn-light dropdown-toggle m-1"
                 list="indicatorList"
                 placeholder="Choose indicator..."
                 onChange={(e) =>
@@ -190,26 +213,28 @@ function Search() {
               ></input>
               <datalist id="indicatorList">{indicatorDropDown()}</datalist>
             </Container>
-            <Container>
-              <label>Year range:</label>
-              <select
-                className="btn btn-light dropdown-toggle"
-                defaultValue={0}
-                onChange={(e) => setStart(e.target.value)}
-              >
-                {yearDropDown(parseInt(end), 1980, "Start")}
-              </select>
-              <select
-                className="btn btn-light dropdown-toggle"
-                defaultValue={0}
-                onChange={(e) => setEnd(e.target.value)}
-              >
-                {yearDropDown(2021, parseInt(start), "End")}
-              </select>
+            <Container className="d-flex flex-column align-items-center text-center">
+              <label className="p-2">Year range:</label>
+              <Container className="d-flex flex-row align-items-center text-center">
+                <select
+                  className="btn btn-light dropdown-toggle p-2 m-1"
+                  defaultValue={0}
+                  onChange={(e) => setStart(e.target.value)}
+                >
+                  {yearDropDown(parseInt(end), 1980, "Start")}
+                </select>
+                <select
+                  className="btn btn-light dropdown-toggle p-2 m-1"
+                  defaultValue={0}
+                  onChange={(e) => setEnd(e.target.value)}
+                >
+                  {yearDropDown(2021, parseInt(start), "End")}
+                </select>
+              </Container>
             </Container>
           </Container>
-          <Button className="btn btn-secondary m-2">See results</Button>{" "}
-        </Form>{" "}
+          <Button className="btn btn-secondary mt-3">See results</Button>{" "}
+        </Form>
       </Container>
     </div>
   );
--- server/routers/users.js
+++ server/routers/users.js
@@ -9,68 +9,68 @@ const { Pool, Client } = require("pg");
 //router.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 const pool = new Pool({
-	user: "postgres",
-	host: "localhost",
-	database: "worldbank",
-	password: null,
-	port: 5432,
+  user: "postgres",
+  host: "localhost",
+  database: "worldbank",
+  password: null,
+  port: 5432,
 });
 
 (async function () {
-	res = await pool.connect();
-	return res;
+  res = await pool.connect();
+  return res;
 })();
 
 // define the home page route
 router.post("/", async function (req, res) {
-	const client = await pool.connect();
+  const client = await pool.connect();
 
-	const { username, password } = await req.body;
-	const salt = await bcrypt.genSalt(8);
-	const passwordEncrypted = await bcrypt.hash(password, salt);
-	const duplicate = await client.query(
-		`SELECT username FROM users WHERE username=$1`,
-		[username]
-	);
-	console.log(duplicate.rows.length);
-	if (duplicate.rows.length !== 0) {
-		res.status(400).json({ Message: "Error" }, 400);
-		console.log("shit");
-	} else {
-		res.status(200).json({ Message: "Works" }, 200);
+  const { username, password } = await req.body;
+  const salt = await bcrypt.genSalt(8);
+  const passwordEncrypted = await bcrypt.hash(password, salt);
+  const duplicate = await client.query(
+    `SELECT username FROM users WHERE username=$1`,
+    [username]
+  );
+  console.log(duplicate.rows.length);
+  if (duplicate.rows.length !== 0) {
+    res.status(400).json({ Message: "Error" }, 400);
+    console.log("shit");
+  } else {
+    res.status(200).json({ Message: "Works" }, 200);
 
-		await client.query(`INSERT INTO users(username,password) VALUES($1,$2)`, [
-			username,
-			passwordEncrypted,
-		]);
+    await client.query(`INSERT INTO users(username,password) VALUES($1,$2)`, [
+      username,
+      passwordEncrypted,
+    ]);
 
-		client.release();
-	}
+    client.release();
+  }
 });
 
 router.post("/verify", async function (req, res) {
-	const client = await pool.connect();
+  const client = await pool.connect();
 
-	const { username, password } = await req.body;
-	const hash = await client.query(
-		`SELECT password FROM users WHERE username=$1`,
-		[username]
-	);
-	console.log(hash.rows[0]);
-	if (hash.rows[0]) {
-		const hashing = hash.rows[0].password;
+  const { username, password } = await req.body;
+  const hash = await client.query(
+    `SELECT password FROM users WHERE username=$1`,
+    [username]
+  );
+  console.log(hash.rows[0]);
+  if (hash.rows[0]) {
+    const hashing = hash.rows[0].password;
 
-		const result = await bcrypt.compare(password, hashing);
-		if (result) {
-			res.json({ status: "loggedIn" }, 200);
-		} else {
-			res.json({ status: "Incorrect Password" }, 400);
-		}
-	} else {
-		res.json({ status: "Incorrect Username!" }, 400);
-	}
+    const result = await bcrypt.compare(password, hashing);
+    if (result) {
+      res.json({ status: "loggedIn" }, 200);
+    } else {
+      res.json({ status: "Incorrect Password" }, 400);
+    }
+  } else {
+    res.json({ status: "Incorrect Username!" }, 400);
+  }
 
-	client.release();
+  client.release();
 });
 
 module.exports = router;
