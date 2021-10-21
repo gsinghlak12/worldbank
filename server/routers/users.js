b50aeebe6bd9636245e50ef179e983e79c3158db
Sang Ta <sang_ta_sigma@Sangs-MacBook-Pro.local>
2021-10-21 16:23:15 +0100

    Merge errors

--- server/routers/users.js
+++ server/routers/users.js
@@ -24,7 +24,6 @@ const pool = new Pool({
 router.post("/", async function (req, res) {
   const client = await pool.connect();
 
-<<<<<<< HEAD
   const { username, password } = await req.body;
   const salt = await bcrypt.genSalt(8);
   const passwordEncrypted = await bcrypt.hash(password, salt);
@@ -32,25 +31,10 @@ router.post("/", async function (req, res) {
     `SELECT username FROM users WHERE username=$1`,
     [username]
   );
-  console.log(duplicate.rows.length);
   if (duplicate.rows.length !== 0) {
     res.status(400).json({ Message: "Error" }, 400);
-    console.log("shit");
   } else {
     res.status(200).json({ Message: "Works" }, 200);
-=======
-	const { username, password } = await req.body;
-	const salt = await bcrypt.genSalt(8);
-	const passwordEncrypted = await bcrypt.hash(password, salt);
-	const duplicate = await client.query(
-		`SELECT username FROM users WHERE username=$1`,
-		[username]
-	);
-	if (duplicate.rows.length !== 0) {
-		res.status(400).json({ Message: "Error" }, 400);
-	} else {
-		res.status(200).json({ Message: "Works" }, 200);
->>>>>>> main
 
     await client.query(`INSERT INTO users(username,password) VALUES($1,$2)`, [
       username,
@@ -64,24 +48,13 @@ router.post("/", async function (req, res) {
 router.post("/verify", async function (req, res) {
   const client = await pool.connect();
 
-<<<<<<< HEAD
   const { username, password } = await req.body;
   const hash = await client.query(
     `SELECT password FROM users WHERE username=$1`,
     [username]
   );
-  console.log(hash.rows[0]);
   if (hash.rows[0]) {
     const hashing = hash.rows[0].password;
-=======
-	const { username, password } = await req.body;
-	const hash = await client.query(
-		`SELECT password FROM users WHERE username=$1`,
-		[username]
-	);
-	if (hash.rows[0]) {
-		const hashing = hash.rows[0].password;
->>>>>>> main
 
     const result = await bcrypt.compare(password, hashing);
     if (result) {
